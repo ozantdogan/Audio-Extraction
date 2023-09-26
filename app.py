6c93a7335972198dbc4ad67125e8162cd7eb4d38
Ozan Doğan <ozantdogan@outlook.com>
2023-09-26 03:40:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,5 +33,5 @@ if __name__ == '__main__':
     if not os.environ.get("WERKZEUG_RUN_MAIN"):
         webbrowser.open_new('http://127.0.0.1:5000/apidocs')
 
-    app.run(host="127.0.0.1", port=5000)
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
+
